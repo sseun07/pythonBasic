4705ccfc8dc0b6dfda8ac9d1353ac98fcc25ccf6
sseun07 <85442104+sseun07@users.noreply.github.com>
2021-06-07 23:53:42 +0900

    Update Lecture1.py

--- Lecture1.py
+++ Lecture1.py
@@ -1,1 +1,53 @@
+'''
 print(15//5)#이 연산자는 몫을 나타낸다
+print(15%5)#이 연산자는 모듈로이며 나머지를 나타낸다
+print(16/5, end='i')#이 소수점이 붙는다. 
+print(16//5, end='')
+print(10/2, end='')
+'''
+'''
+date type
+#float='소수'
+#int='정수' (integer)
+#boolean= 'T/F' >> "if statement" 에서 배우게 될것 
+#complex= '복소수'-a+bi (i=허수/sqrt(-1))
+#string= 일단 타입이 아니고 우리가 쓰는 글들을 부르는 
+#byte(용량)는 한정적이다 >>8bit(0과 1로 연산)=1byte 
+# >> 이진법=0&1로 이루어짐 (전류가 흐름-1, 전류흐름x=0)
+'''
+
+#'''/"""
+#quotation=hidden notes/comments
+#'''/"""
+
+#/n= new line (trying not to go over all the way across the page)
+#, end='' >> 뒤에다가 ''를 붙여라 (could be do not attach anything when blank)
+
+#char= part of string=one character
+#'ab' (group of char>> char을 group 시켜주는 'LIST') 에서 'a' or 'b' is a 'char' 
+'''
+10진법을 2진법으로 전환할때:
+8 8*10^0
+17
+1*10^1+7*10^0
+7
+1*2^2+1*2^1+1*2^0
+111
+6
+2진법 (binary): 1*2^2+1*2^1+0*10^0 >> 110
+3진법 (tertiary): 2*3^1+0*3^0 >> 20
+'''
+
+'''
+print('ab'+'bc') >> there is only addition, when played=merged as 'abbc'
+'''
+fuhgghj = 7
+seung = 5.2
+print(seung*fuhgghj)
+
+#근의 공식/ quadratic formula 
+a=-1
+b=4
+c=4
+print((-1*b+(b^2-4*a*c)**(1/2))/(2*a))
+print((-1*b-(b^2-4*a*c)**(1/2))/(2*a))
