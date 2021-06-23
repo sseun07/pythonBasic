2c97e254d4ab12dc1f31289a7afa02d9722eeb8f
sseun07 <85442104+sseun07@users.noreply.github.com>
2021-06-23 23:31:43 +0900

    Create Lecture3.py

---
+++ Lecture3.py
@@ -0,0 +1,23 @@
+'''
+Where do you use for-loop? 
+RANGE-함수
+- range(a,b) *a is optional (is inclusive) // a 보다 크거나 같고, b 보다는 작은것 
+- create list
+'''
+for each in range(16,25): 
+    print(each)
+
+l=[1,2,3,7,10,20,35]
+print(l[0])
+for each in range(len(l)):
+    print(l[each])
+
+'''
+len=length of the list
+'''
+'''
+NEGATIVE INDEX 
+l[-1] : 뒤에서 첫번째 l[-2] : 뒤에서 두번째
+print(l[-1]) 
+'''
+#HW: for loop 은 어디에 쓰는지 RESEARCH by (2021.06.24 7pm)
